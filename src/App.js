2a4bdbd8090292525c4cd692bbc30d4b95ed7c6b
Suleiman Olaide <olaide.suleiman96@gmail.com>
2022-09-14 15:20:50 -0700

    Add app components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+/* eslint-disable import/extensions */
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/calculator';
+import Home from './components/Home';
+import Quotes from './components/Quotes';
+import Nav from './components/Nav';
 
 const App = () => (
-  <Calculator />
+  <div>
+    <Nav />
+    <Routes>
+      <Route path="calculator" element={<Calculator />} />
+      <Route path="quotes" element={<Quotes />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </div>
 );
 
 export default App;
